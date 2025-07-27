542d98603fd07c9d6f29361f1467538d3f3d2ce4
Adam Trager <hello@adamtrager.com>
2025-07-26 20:09:07 -0400

    remove happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'happy-dom',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'dist/', 'src/test/']
-    },
+    // environment: 'happy-dom',
     testTimeout: 30000, // 30 seconds for live API calls
     hookTimeout: 30000
   }
